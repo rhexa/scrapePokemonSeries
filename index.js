d2f0cebd7b752ba54a0cf7542927f0d00314c482
rhexa <akun.kripitit@gmail.com>
2021-08-03 13:45:55 +0300

    process.exit

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { exec } = require("child_process");
 
 // fi, us
 const language = {
-        fi: 'https://www.pokemon.com/api/pokemontv/v2/channels/fi/',
-        us: 'https://www.pokemon.com/api/pokemontv/v2/channels/us/' 
+    fi: 'https://www.pokemon.com/api/pokemontv/v2/channels/fi/',
+    us: 'https://www.pokemon.com/api/pokemontv/v2/channels/us/'
 };
 
 const baseUrl = language.fi;
@@ -39,10 +39,10 @@ function runVlc(url) {
     })
 }
 
-const {inspect} = require('util')
+const { inspect } = require('util')
 
-function insLog(obj, depth=1) {
-	console.log(inspect(obj, {depth: depth}));
+function insLog(obj, depth = 1) {
+    console.log(inspect(obj, { depth: depth }));
 }
 
 async function getData() {
@@ -50,21 +50,33 @@ async function getData() {
         const result = await fetch(baseUrl);
         const data = await result.json();
         //insLog(data);
-        //data.forEach(r => insLog(r));
-        //insLog(data[7].channel_id);
-	const season1 = data[8].media;
-        const streamUrl = await season1.map((e,index) => { 
-            console.log({ episode: e.episode, title: e.title});
-            return {episode : e.episode, stream_url : e.stream_url}
-        })
+        const seasons = await data.map((r, i) => {
+            return { index: i, season_name: r.channel_name, media: r.media}
+        });
+        
+        while (true) {
+            seasons.forEach(e => console.log(`${e.index+1}. ${e.season_name}`));
+            let selectedSeason = await readLine("Select the season index number:   (q or x to exit)");
 
-	while(true) {
-        	let episode = await readLine("Select the episode to watch");
-        	if (episode == "q" || episode == "x") break;
-		// console.log(streamUrl[episode-1].stream_url);
-        	if (streamUrl.some(a => a.episode == episode)) runVlc(streamUrl[episode-1].stream_url);
-			
-	}
+            if (selectedSeason == "q" || selectedSeason == "x") process.exit();
+
+            const season = seasons[selectedSeason - 1].media;
+
+            const streamUrl = await season.map((e, index) => {
+                console.log({ episode: e.episode, title: e.title });
+                return { episode: e.episode, stream_url: e.stream_url }
+            })
+    
+            while (true) {
+                let episode = await readLine("Select the episode to watch:  (b to go back, q or x to exit)");
+                if (episode == 'b') break;
+                if (episode == "q" || episode == "x") process.exit();
+                // console.log(streamUrl[episode-1].stream_url);
+                if (streamUrl.some(a => a.episode == episode)) runVlc(streamUrl[episode - 1].stream_url);
+    
+            }
+        }
+        //insLog(data[7].channel_id);
     } catch (error) {
         console.log('Error : ' + error);
     }
